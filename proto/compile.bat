7270a9134566e9c141d5bbaa2a5065f062a14a1f
onzlak42 <onzlak42@gmail.com>
2016-08-22 12:24:50 +0300

    check changes POGOProtos src

--- proto/compile.bat
+++ proto/compile.bat
@@ -1,6 +1,9 @@
 SET path=%cd%;%path%
 cd ..\POGOProtos\src
+changes_checker.exe "POGOProtos" "..\..\proto\crc.json"
+if errorlevel 1 (
 proto_compiler.exe "POGOProtos" "..\..\proto\proto_cpp"
+)
 
 
 
